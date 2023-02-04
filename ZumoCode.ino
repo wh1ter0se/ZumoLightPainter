cbf1d056a08581a4d89dee29b29a6037793338f3
wh1ter0se <rckreischer@gmail.com>
2023-02-04 15:34:13 -0500

    Added forward(inches, motorSpeed).

--- ZumoCode.ino
+++ ZumoCode.ino
@@ -1,15 +1,25 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
+#include <cmath>
 
 #define getXY 0
 #define printing 1
 #define printDone 2
 
-Zumo32U4LCD display;
 Zumo32U4ButtonA buttonA;
 Zumo32U4ButtonB buttonB;
 Zumo32U4ButtonC buttonC;
+Zumo32U4LCD display;
+Zumo32U4Encoders encoders;
+Zumo32U4Motors motors;
 
+// Encoder constants
+const double tread_diam_in = 1.53543; // total height of treads, verified in CAD
+const double ticks_per_rev = 12 * 75.81; // this may be 51.45, 75.81, or 100.37
+const double in_per_rev = M_PI * tread_diam_in; // circumference = 2*pi*r = pi*D
+const double ticks_per_in = ticks_per_rev / in_per_rev;
+
+// Menu vars
 uint8_t x;
 uint8_t y;
 uint8_t state;
@@ -55,3 +65,38 @@ void loop() {
       delay(1000);
   }
 }
+
+void drawImage(double width_in, double height_in, int rows);
+
+void turnCW(double degrees);
+void turnCCW(double degrees);
+
+/**
+ * Moves forward specified distance at constant, specified motor speed. 
+ * Moves until at least one encoder hits its setpoint.
+ * 
+ * Paramaters
+ * inches     : distance to travel in inches (max 120in)
+ * motorSpeed : motor rate to travel at (0-400, unitless)
+*/
+void forward(double inches, int motorSpeed = 150) {
+  double setpoint = ticks_per_in * inches;
+
+  encoders.getCountsAndResetLeft(); // set counts to 0 (overflows at 32767 ticks, >10ft)
+  encoders.getCountsAndResetRight();
+  
+  motors.setSpeeds(motorSpeed); // activate motors
+
+  bool transit = true;
+  while (transit) {
+    int delta_L = encoders.getCountsLeft(); // ticks travelled
+    int delta_R = encoders.getCountsRight();
+    int error_L = setpoint - delta_L; // ticks to setpoint (negative if passed)
+    int error_R = setpoint - delta_R;
+    if ((error_L<=0) || (error_R<=0)) { transit = false; }
+  }
+
+  motors.setSpeeds(0); // stop motors
+}
+
+//void paintLine(double inches, );
