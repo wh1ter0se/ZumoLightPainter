cf3c1851f7bc336ea36b541636cb2dac25e62467
Damean Ewan <djewan@mtu.edu>
2023-02-07 21:38:10 -0500

    As is code

--- ZumoCode.ino
+++ ZumoCode.ino
@@ -45,6 +45,7 @@ void turnCCW(double degrees, int motorSpeed = 100);
 void paintLine(double inches, char pixels[54], bool leftToRight = true, int motorSpeed = 150);
 void drawImage(double width_in, double height_in, int rows);
 void forward(double inches, int motorSpeeds = 150);
+void backward(double inches, int motorSpeeds = 150);
 void turnCW_mag(double degrees, int motorSpeed = 100);
 double getHeading();
 
@@ -95,68 +96,7 @@ char image[36][54] = {
  * 
  * Pass true as printData argument for method to print debugging info.
  */
-void calibrate(bool printData) {
-  xyzData min = {30000, 30000, 30000};
-  xyzData max = {-30000, -30000, -30000};
 
-  for(int i = 0; i<5;i++)
-  {
-    //reads the magnetometer data and stores it in point
-    imu.readMag();
-    xyzData point = {imu.m.x, imu.m.y, imu.m.z};
-
-    //check max and min for x,y,z
-    if(point.x > max.x) {
-      max.x = point.x;
-    }
-    if(point.x < min.x) {
-      min.x = point.x;
-    }
-    if(point.y > max.y) {
-      max.y = point.y;
-    }
-    if(point.y < min.y) {
-      min.y = point.y;
-    }
-    if(point.z > max.z) {
-      max.z = point.z;
-    }
-    if(point.z < min.z) {
-      min.z = point.z;
-    }
-
-    //print info if debugging data is on
-    if(printData)
-    {
-      char strBuf[100];
-      sprintf(strBuf,"X: min=%-6d max=%-6d \nY: min=%-6d max=%-6d \nZ:min=%-6d max=%-6d", 
-              min.x, max.x, min.y, max.y, min.z, max.z);
-      Serial.println(strBuf);
-      Serial.println();
-    }
-
-    delay(50);
-  }
-  //wait until button A is not being pressed
-
-
-  //calculate the error(center point) based on max and min for each value
-  errorGlobal.x = (max.x + min.x) / 2;
-  errorGlobal.y = (max.y + min.y) / 2;
-  errorGlobal.z = (max.z + min.z) / 2;
-
-  //print final info if debugging data is on
-  if(printData) {
-    Serial.println("\nFINAL DATA:");
-    char strBuf2[150];
-    sprintf(strBuf2,"X: min=%-6d max=%-6d err=%-6d \nY: min=%-6d max=%-6d err=%-6d \nZ:min=%-6d max=%-6d err=%-6d", 
-            min.x, max.x, errorGlobal.x, min.y, max.y, errorGlobal.y, min.z, max.z, errorGlobal.z);
-    Serial.println(strBuf2);
-  }
-
-  delay(1000);
-
-}
 
 void printStuff() { //Prints the menu, saves a little space to have it as a function
   display.clear();
@@ -181,7 +121,6 @@ void setup() {
   state = 0;
   printStuff();
   Wire.begin();
-  calibrate(false);
 }
 
 
@@ -205,14 +144,10 @@ void loop() {
       break;
     case 1:
       delay(1000);
-      //drawImage(50,34,36);
-      turnCW_mag(90);
+      drawImage(36,24,36);
       state++;
     case 2:
       delay(500);
-      ledGreen(1);
-      ledYellow(1);
-      ledRed(1);
       break;
   }
 }
@@ -227,11 +162,14 @@ void loop() {
  * motorSpeed : motor rate to travel at (0-400, unitless)
 */
 void turnCW(double degrees, int motorSpeed = 100) {
+  ledGreen(0);
+  ledYellow(0);
+  ledRed(0);
   turnSensorReset();
   delay(500);
   turnSensorUpdate();
   int init_angle = ((((int32_t)turnAngle>>16)*360)>>16);
-  int setpoint = init_angle + ((((int32_t)turnAngle90>>16)*360)>>16);
+  int setpoint = init_angle + ((((int32_t)(turnAngle1*degrees)>>16)*360)>>16);
   display.clear();
   display.print(F("CW Turn"));
 
@@ -246,36 +184,15 @@ void turnCW(double degrees, int motorSpeed = 100) {
   motors.setSpeeds(0, 0); // stop motors
 }
 
-void turnCW_mag(double degrees, int motorSpeed = 100) {
-  setpoint_heading += degrees;
-  while(setpoint_heading>360)
-    setpoint_heading -= 360;
-  //void hasOverflow = (setpoint >= 360.0); 
-
-  double kP = 50.0;
-  double tolerance = 3.0; // degrees
-
-  // motors.setSpeeds(motorSpeed, -motorSpeed); // activate motors
-  bool transit = true;
-  while (transit) {
-    double real_heading = getHeading();
-    double angle_error = setpoint_heading - real_heading;
-    
-    if (abs(angle_error) <= tolerance) { transit = false; }
-
-    double motor_diff = kP * angle_error;
-    motors.setSpeeds(motor_diff, -motor_diff);
-  }
-
-  motors.setSpeeds(0, 0); // stop motors
-}
-
 void turnCCW(double degrees, int motorSpeed = 100) {
+  ledGreen(0);
+  ledYellow(0);
+  ledRed(0);
   turnSensorReset();
   delay(500);
   turnSensorUpdate();
   int init_angle = ((((int32_t)turnAngle>>16)*360)>>16);
-  int setpoint = init_angle - ((((int32_t)turnAngle90>>16)*360)>>16);
+  int setpoint = init_angle - ((((int32_t)(turnAngle1*degrees)>>16)*360)>>16);
   display.clear();
   display.print(F("CCW Turn"));
 
@@ -304,7 +221,6 @@ void forward(double inches, int motorSpeed = 150) {
   delay(500);
   turnSensorUpdate();
   int init_angle = ((((int32_t)turnAngle>>16)*360)>>16);
-  int setpoint2 = init_angle - ((((int32_t)turnAngle90>>16)*360)>>16);
   encoders.getCountsAndResetLeft(); // set counts to 0 (overflows at 32767 ticks, >10ft)
   encoders.getCountsAndResetRight();
   
@@ -313,14 +229,40 @@ void forward(double inches, int motorSpeed = 150) {
   bool transit = true;
   while (transit) {
     turnSensorUpdate();
-    int errorAngle = ((((int32_t)turnAngle>>16)*360)>>16);
+    int errorAngle =  ((((int32_t)turnAngle>>16)*360)>>16);
     Serial.print(errorAngle);
     Serial.print(F("\n"));
-    motors.setSpeeds((motorSpeed+errorAngle*5),(motorSpeed-errorAngle*5));
+    motors.setSpeeds((motorSpeed+errorAngle),(motorSpeed-errorAngle));
     int delta_L = encoders.getCountsLeft(); // ticks travelled
     int delta_R = encoders.getCountsRight();
     int error_L = setpoint - delta_L; // ticks to setpoint (negative if passed)
     int error_R = setpoint - delta_R;
+    if ((error_L<=0) || (error_R<=0)) { transit = false; }
+  }
+
+  motors.setSpeeds(0,0); // stop motors
+}
+
+void backward(double inches, int motorSpeed = 150) {
+  ledGreen(0);
+  ledYellow(0);
+  ledRed(0);
+  double setpoint = ticks_per_in * inches;
+  turnSensorReset();
+  delay(500);
+  turnSensorUpdate();
+  encoders.getCountsAndResetLeft(); // set counts to 0 (overflows at 32767 ticks, >10ft)
+  encoders.getCountsAndResetRight();
+  
+  motors.setSpeeds(-motorSpeed,-motorSpeed); // activate motors
+
+  bool transit = true;
+  while (transit) {
+    turnSensorUpdate();
+    int delta_L = -(encoders.getCountsLeft()); // ticks travelled
+    int delta_R = -(encoders.getCountsRight());
+    int error_L = (setpoint - delta_L); // ticks to setpoint (negative if passed)
+    int error_R = (setpoint - delta_R);
     if ((error_L<=0) || (error_R<=0)) { transit = false; }
   }
 
@@ -348,16 +290,16 @@ void paintLine(double inches, char pixels[54], bool leftToRight, int motorSpeed 
   encoders.getCountsAndResetLeft(); // set counts to 0 (overflows at 32767 ticks, >10ft)
   encoders.getCountsAndResetRight();
   
-  motors.setSpeeds(motorSpeed,motorSpeed); // activate motors
+  if(leftToRight)
+    motors.setSpeeds(motorSpeed,motorSpeed); // activate motors
+  else
+    motors.setSpeeds(-motorSpeed,-motorSpeed);
   Serial.print(F("\n"));
 
   bool transit = true;
   while (transit) {
     turnSensorUpdate();
     int errorAngle = ((((int32_t)turnAngle>>16)*360)>>16);
-    Serial.print(errorAngle);
-    Serial.print(F("\n"));
-    motors.setSpeeds((motorSpeed+errorAngle*5),(motorSpeed-errorAngle*5));
     int delta_L = encoders.getCountsLeft(); // ticks travelled
     int delta_R = encoders.getCountsRight();
     int error_L = setpoint - delta_L; // ticks to setpoint (negative if passed)
@@ -414,56 +356,10 @@ void paintLine(double inches, char pixels[54], bool leftToRight, int motorSpeed 
  * rows      : number of rows to zigzag
 */
 void drawImage(double width_in, double height_in, int rows) {
-  init_heading = getHeading();
-  setpoint_heading = init_heading;
-
-  bool leftToRight;
+  bool leftToRight = true;
   for(int i = 0;i<rows;i++){
-    leftToRight = !(i%2);
     paintLine(width_in,image[i],leftToRight);
-    if(leftToRight){
-      turnCW(90);
-      forward((height_in/rows));
-      turnCW(90);
-    }
-    else{
-      turnCCW(90);
-      forward((height_in/rows));
-      turnCCW(90);
-    }
-  }
-}
+    backward(36);
 
-double getHeading() {
-  //reads in data from magnetometer and calculates corrected point doing actual - error
-  imu.readMag();
-  xyzData correctedPoint = {imu.m.x - errorGlobal.x, imu.m.y - errorGlobal.y, imu.m.z - errorGlobal.z};
-
-  double heading = atan2( correctedPoint.x, correctedPoint.y) - PI / 180; //calculates the heading in radians based on the following equation
-  //the following if, else if statements shift the heading to an interval of 0 to 2*pi
-  if (heading > PI) 
-  {
-    heading -= (2 * PI);
   }
-  else if (heading < -PI) 
-  {
-    heading += (2 * PI);
-  }
-  else if (heading < 0) 
-  {
-    heading += 2 * PI;
-  }
-  heading *= (180 / PI); //rad to degrees
-  heading = 360 - heading; // invert
-  int rotateAngle = 90;
-  heading = heading + rotateAngle; //rotate to have 0 = north
-  if(heading > 360)
-  {
-    heading -= 360;
-  }
-  else if(heading < 0)
-  {
-    heading += 360;
-  }
-  return heading;  
 }
